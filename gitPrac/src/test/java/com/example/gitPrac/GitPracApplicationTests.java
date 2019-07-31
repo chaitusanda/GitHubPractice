a2d5442e7c4797d36a5a32023df69177e8fdbed1
chaitusanda <42526144+chaitusanda@users.noreply.github.com>
2019-07-31 06:33:08 +0530

    Initial setup V.0
    Initial version
    just created the mvn project with cloud,

---
+++ gitPrac/src/test/java/com/example/gitPrac/GitPracApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.gitPrac;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitPracApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
